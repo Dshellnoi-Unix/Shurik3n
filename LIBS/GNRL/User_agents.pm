4e2b4fec02a75c133e43c0dcdf58d818356d7d4b
Dshellnoi-Unix <templesec0day@gmail.com>
2013-03-25 02:14:27 +0000

    New Update

--- LIBS/GNRL/User_agents.pm
+++ LIBS/GNRL/User_agents.pm
@@ -1,39 +1,22 @@
-###
-#           CLASE:User_agents
+####
+#  _________.__                 .__ __    ________         
+# /   _____/|  |__  __ _________|__|  | __\_____  \  ____  
+# \_____  \ |  |  \|  |  \_  __ \  |  |/ /  _(__  < /    \ 
+# /        \|   Y  \  |  /|  | \/  |    <  /       \   |  \
+#/_______  /|___|  /____/ |__|  |__|__|_ \/______  /___|  /
+#        \/      \/                     \/       \/     \/ 
 #
-#          DESCRIPCION : 
-#                       Devuelve un user agent para configurar en los exploits
-#                       y en los modulos auxiliares y lista todos los user agents
-#                       tambien cuenta todos los user agents
+#                    Dshellnoi Unix  2013
 #
-#          LLAMANDO E INTANCIANDO LA CLASE :
-#                       use LIBS::GNRL::User_agents ;
-#                       $user_agent =  LIBS::GNRL::User_agents->new();
-#                       
-#          USANDO LOS METODOS :
-#
-#                       Devuelve un user agent listo para configurar una peticion en un exploiit
-#                       o en un modulo auxiliar 
-#
-#                       print $user_agent->show_ua(12);
-#
-#          LISTA TODOS LOS USER AGENTS :
-#
-#                       $user_agent->lista_ua();
-#
-#
-#
-#          CONFIGURA UN USER AGENT:
-#                       print $user_agent->ua_configure("MI USER AGENT");
-#
-#
+#           CLASE     :  LIBS::GNRL::User_agents
 #
 #          CREADA:                   12-03-2013                                
 #          ESCRITA POR:              Dshellnoi_Unix
 #          ULTIMA ACTUALIZACION:     17-03-2013
+#
+#          LICENCIA: 
+#          
 #####
-
-
 
 
 
@@ -51,7 +34,7 @@ $VERSION = '0.02';
 ##
 
 my @ua ;
-$ua[0]="Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)" ;
+$ua[0] ;
 $ua[1]="Googlebot/2.1 ( http://www.googlebot.com/bot.html)" ;
 $ua[2]="Mozilla/2.0 (compatible; MSIE 3.02; Update a; AOL 3.0; Windows 95)" ;
 $ua[3]="Mozilla/3.0 (compatible; Opera/3.0; Windows 3.1) v3.1" ;
@@ -64,6 +47,7 @@ $ua[9]="Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)" ;
 $ua[10]="Opera/9.20 (Windows NT 6.0; U; en)" ;
 $ua[11]="Mozilla/2.0 (compatible; Ask Jeeves)" ;
 $ua[12]="Googlebot-Image/1.0 ( http://www.googlebot.com/bot.html)" ;
+$ua[13]="Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)" ;
 
 
 
@@ -103,16 +87,10 @@ if (! $ua[$self->{'num'}]){
  
 my $test =  LIBS::GNRL::Colores->new();  
    $test->pinta("ROJO", "[-]I do not recognize the user agent\n"); 
-   #
-   # CONTROLAR EL FLUJO SI FALLA
-   #
-   exit ;
+   goto SHELL ; 
 }
-#
-# IMPRIME EL USER AGENT
-#
-my $test =  LIBS::GNRL::Colores->new();  
-   $test->pinta("AMARILLO", $ua[$self->{'num'}]); 
+ 
+return $ua[$self->{'num'}]; 
  
 }
 
@@ -123,28 +101,13 @@ my $test =  LIBS::GNRL::Colores->new();
 
 sub lista_ua
 {
-for(my $i=0; $i <= $#ua ; $i++ ){
+
+for(my $i=1; $i <= $#ua ; $i++ ){
      my $shw = "[$i]".$ua[$i]."\n";
      my $test =  LIBS::GNRL::Colores->new();  
         $test->pinta("AMARILLO", $shw); 
     }
 }
 
-
-##
-#
-#
-#CONFIGURA UN USER AGENT MANUAL
-#
-##
-
-sub ua_configure
-{
-
-my($self,$configure) = @_;
-$self->{'configure'} = $configure ;
-return $self->{'configure'} ;
-
-}
 
 1;
