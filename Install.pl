599f209777d069e406c54694665a454695a07b48
Dshellnoi-Unix <templesec0day@gmail.com>
2013-03-29 02:35:28 +0000

    Instalador

---
+++ Install.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+####
+#  _________.__                 .__ __    ________         
+# /   _____/|  |__  __ _________|__|  | __\_____  \  ____  
+# \_____  \ |  |  \|  |  \_  __ \  |  |/ /  _(__  < /    \ 
+# /        \|   Y  \  |  /|  | \/  |    <  /       \   |  \
+#/_______  /|___|  /____/ |__|  |__|__|_ \/______  /___|  /
+#        \/      \/                     \/       \/     \/ 
+#
+#                    Dshellnoi Unix  2013
+#              ============================        
+#                 [[[[[[[[Instal]]]]]]]]
+#              ============================ 
+#                                                            
+#   CREADO POR   :      Dshellnoi Unix   
+#   CORREO       :      templesec0day@gmail.com                    
+#   FECHA:              28-3-2013                             
+#   ACTUALIZACION:       
+#   VERSION      :      0.1                            
+#                                                                
+#   LICENCIA:                                                 
+#            SUJETO A LA LICENCIA SHURIK3N           
+####
+
+use LIBS::GNRL::Colores ;
+
+
+$banner ='
+  _________.__                 .__ __    ________         
+ /   _____/|  |__  __ _________|__|  | __\_____  \  ____  
+ \_____  \ |  |  \|  |  \_  __ \  |  |/ /  _(__  < /    \ 
+ /        \|   Y  \  |  /|  | \/  |    <  /       \   |  \
+/_______  /|___|  /____/ |__|  |__|__|_ \/______  /___|  /
+        \/      \/                     \/       \/     \/ 
+
+                 Shurik3n    Installer
+';
+
+
+ my $color =  LIBS::GNRL::Colores->new(); 
+    $color->pinta("AMARILLO", "$banner\n") ;
+
+
+$user = $< ;
+
+if($user !=0 ){
+
+ $color->pinta("ROJO", "[-]Be root my friend\n") ;
+exit ;
+
+}
+
+
+my @modulos = (
+               "Module::Load",
+               "Module::Util",
+               "Term::Screen",
+               "Switch" 
+              );
+
+
+
+for(my $i = 0 ; $i <=  $#modulos ; $i++ ){
+
+ $color->pinta("AMARILLO", "[+]installing $modulos[$i] \n") ;
+sleep 1;
+
+system("cpan -e install $modulos[$i]");
+
+sleep 1;
+
+}
+
+$color->pinta("AMARILLO", "[+]Congratulations, you can use Shurik3n\n") ;
+
